5d0a71c2736f9d40125974d5aeec3454ff5cffe6
txleadfoot00 <106290919+txleadfoot00@users.noreply.github.com>
2023-01-11 18:38:30 -0800

    commit tree f21f9fc9a98b79475573ccb1ac8e3a8b82c42573

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Code your solution here
+function findMatching(arr, name) {
+    return arr.filter((str) => str.toLowerCase().includes(name.toLowerCase()));
+}
+
+function fuzzyMatch(arr, frag) {
+    return arr.filter((str) => str.toLowerCase().startsWith(frag.toLowerCase()))
+}
+
+function matchName(arr, drname) {
+    return arr.filter((rec) => rec.name === drname);
+}
